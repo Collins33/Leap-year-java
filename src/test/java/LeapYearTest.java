83d514ef560a1a5c0d06ebbb8e8522ba5f8a7606
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-03 12:40:11 +0300

    I have added a third test to return true for years divisible by 400

--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -3,21 +3,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LeapYearTest {
-  @Test
-  public void isLeapYear_forNumbersDivisibleByFour_true(){
-    LeapYear leapYear=new LeapYear();
-    assertEquals(true, leapYear.isLeapYear(2012));
+@Test
+public void isLeapYear_forNumbersDivisibleByFour_true(){
+        LeapYear leapYear=new LeapYear();
+        assertEquals(true, leapYear.isLeapYear(2012));
 
-  }
-  @Test
-  public void isLeapYear_forNumbersNotDivisibleByFour_false(){
-    LeapYear leapYear=new LeapYear();
-    assertEquals(false, leapYear.isLeapYear(1999));
-  }
-  @Test
-  public void isLeapYear_forNumbersDivisibleByOneHundred_false(){
-    LeapYear leapYear=new LeapYear();
-    assertEquals(false, leapYear.isLeapYear(1900));
-  }
+}
+@Test
+public void isLeapYear_forNumbersNotDivisibleByFour_false(){
+        LeapYear leapYear=new LeapYear();
+        assertEquals(false, leapYear.isLeapYear(1999));
+}
+@Test
+public void isLeapYear_forNumbersDivisibleByOneHundred_false(){
+        LeapYear leapYear=new LeapYear();
+        assertEquals(false, leapYear.isLeapYear(1900));
+}
+@Test
+public void isLeapYear_forNumbersDivisibleByFourHundred_true(){
+  LeapYear leapYear=new LeapYear();
+  assertEquals(true, leapYear.isLeapYear(2000));
+
+}
 
 }
